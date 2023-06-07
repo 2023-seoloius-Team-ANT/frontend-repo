517dd6fbbbb4c296da4e6969effb579dc936b7e0
ohjihwan <dnakrk97@gmail.com>
2023-06-07 16:05:52 +0900

    feat:appbtn
    신청확인, 신청거절 api 연결

--- src/component/appbtn.js
+++ src/component/appbtn.js
@@ -1,7 +1,24 @@
+import { useState, useEffect } from "react";
 import { useNavigate } from "../../node_modules/react-router-dom/dist/index"
-
+import axios from "../../node_modules/axios/index";
 const Appbtn=()=>{
   const navigate = useNavigate();
+  const [accept, setAccept] = useState();
+  const [decline, setDecline] = useState();
+  useEffect(()=> {
+    axios.put("/api/v1/connect/1/accept",{ withCredentials: true, }).then((response)=>{
+      if(response.data){
+        console.log(response.data);
+        setAccept(response.data.result);
+      }
+    });
+    axios.put("/api/v1/connect/1/decline",{ withCredentials: true, }).then((response)=>{
+      if(response.data){
+        console.log(response.data);
+        setDecline(response.data.result);
+      }
+    });
+  },[]);
   return(
     <ul>
       <li>
@@ -10,12 +27,12 @@ const Appbtn=()=>{
         </button>
       </li>
       <li>
-        <button>
+        <button onClick={accept}>
           <img src="images/동그라미.png" alt=""/>
         </button>
       </li>
       <li>
-        <button>
+        <button onClick={decline}>
           <img src="images/엑스.png" alt=""/>
         </button>
       </li>
