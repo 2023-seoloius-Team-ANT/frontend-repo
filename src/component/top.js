7782bc2237466db052117150a4ca3464fa6d51dc
Jeon DaYeon <dyntop15@gmail.com>
2023-06-13 11:01:11 +0900

    fix:header

--- src/component/top.js
+++ src/component/top.js
@@ -1,15 +1,17 @@
 import { useEffect, useState } from 'react';
 import { Routes, Route, Link, useNavigate, useLocation} from 'react-router-dom';
+import { UNSAFE_useScrollRestoration } from '../../node_modules/react-router-dom/dist/index';
 let Top = ()=>{
   const navigate = useNavigate();
   const location = useLocation();
+  const urlLink = location.pathname.split('?')[0]
 
   return(
     <div>
     <header className="header">
     <img className="logo" src={process.env.PUBLIC_URL + "/images/antlogo.png"} alt=""/>
     {
-      location.pathname === '/caregivermain/cal' && '/caregivermain/caregivermypage'? (
+      (urlLink === '/caregivermain/cal' || urlLink === '/caregivermain/caregivermypage')? (
         <button onClick={()=>{navigate('/caregivermain')}}>
           <img src={process.env.PUBLIC_URL + "/images/house.png"} alt=""/>
         </button>
