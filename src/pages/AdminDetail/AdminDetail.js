7049a798e9bca9e7e3ce1cf5d21ccd125f14c879
jim <icekjm1029@gmail.com>
2023-06-21 17:54:46 +0900

    fix: axios수정중
    수정

--- src/pages/AdminDetail/AdminDetail.js
+++ src/pages/AdminDetail/AdminDetail.js
@@ -1,39 +1,55 @@
 import ListGroup from 'react-bootstrap/ListGroup';
 import Badge from 'react-bootstrap/Badge';
 import './admindetail.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import axios from '../../../node_modules/axios/index';
 
 function Kjmadmindetail() {
+  const [stanMember, setStanMember] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('/api/v1/admin/caregiver')
+      .then((response) => {
+        // const newkjm = JSON.stringify(response.data.result);
+        console.log(response.data.result);
+        setStanMember(response.data.result);
+      })
+      .finally(() => {
+        // console.log(stanMember);
+      });
+  }, []);
   const alertClicked = () => {
     alert('You clicked the third ListGroupItem');
   };
 
-  let [stanMember, setStanMember] = useState(['김민수', '박영우']);
-  let [memNo, setMemNo] = useState(0);
-  const [modalShow, setModalShow] = React.useState(false);
+  // const [modalShow, setModalShow] = React.useState(false);
+
+  const [memNo, setMemNo] = useState(0);
 
   return (
     <div className="adminwholething">
       <h1 id="titlejm">
-        <Badge bg="primary" text="light">
-          대기중인 요양사 리스트
+        <Badge bg="dark" text="light">
+          최종 승인 대기중인 요양사 리스트
         </Badge>
       </h1>
-      <ListGroup className="detailjm" defaultActiveKey="#link1">
+      {/* {stanMember[0].name} */}
+      {/* <ListGroup className="detailjm" defaultActiveKey="#link1">
         {stanMember.map((a, i) => {
           return (
             <Button
               key={i}
               className="detailjm1"
-              variant="outline-primary"
+              variant="outline-dark"
               onClick={() => {
                 setModalShow(true);
                 setMemNo(i);
               }}
             >
-              {stanMember[i]}
+              {stanMember[i].name}
             </Button>
           );
         })}
@@ -44,7 +60,7 @@ function Kjmadmindetail() {
           show={modalShow}
           onHide={() => setModalShow(false)}
         />
-      </ListGroup>
+      </ListGroup> */}
     </div>
   );
 }
@@ -59,18 +75,20 @@ function MyVerticallyCenteredModal(props) {
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-          {props.stanMember[props.memNo]}
+          {props.stanMember[props.memNo].name}
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>{props.stanMember[props.memNo]} 상세정보</h4>
+        <h4>{props.stanMember[props.memNo].name} 상세정보</h4>
         <p>친절한 요양사의 기준</p>
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="primary" onClick={props.onHide}>
+        <Button variant="dark" onClick={props.onHide}>
           승인
         </Button>
-        <Button onClick={props.onHide}>거절</Button>
+        <Button variant="dark" onClick={props.onHide}>
+          거절
+        </Button>
       </Modal.Footer>
     </Modal>
   );
