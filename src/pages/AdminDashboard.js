d39217dfdcf75fb28f9298eb9c3079c22e3b6088
parksangjun1196 <parksangjun1196@gmail.com>
2023-06-23 12:13:21 +0900

    park

--- src/pages/AdminDashboard.js
+++ src/pages/AdminDashboard.js
@@ -13,6 +13,7 @@ import DataTable from "examples/Tables/DataTable";
 import AdminModal from '../component/Adminmodal';
 import { useNavigate} from 'react-router-dom';
 import axios from '../../node_modules/axios/index';
+import { useAsync } from 'react-async';
 
 
 
@@ -26,6 +27,7 @@ const caregivercnt = 0;
 var newmem = 0;
 var newcaremem = 0;
 
+
 const AdminDashboard = () => {
   const [Memberdata, setMemberdata] = useState();
   const [Caregiverdata, setCaregiverdata] = useState();
@@ -33,87 +35,77 @@ const AdminDashboard = () => {
   const [Cstmcomplain, setCstmcomplain] = useState();
   const [Membercnt, setMembercnt] = useState();
   const [Caregivercnt, setCaregivercnt] = useState();
+  const [Staticlist, setStaticlist] = useState([{}]);
+
+  const [ConnectAll, setConnectAll] = useState();
+  const [ConenctSuccess, setConenctSuccess] = useState();
+  const [ConnectFail, setConnectFail] = useState();
+  
   
   var memberdata = Memberdata;
   var caregiverdata = Caregiverdata;
   const navigate = useNavigate();
   var newcargiverpaper = Newcargiverpaper;
+  
   var cstmcomplain = Cstmcomplain;
   var matchappr = 0;
   var matchrejc = 0;
+
+  var connectAll = ConnectAll;
+  var conenctSuccess = ConenctSuccess;
+  var connectFail = ConnectFail;
   
   var newcaremem = 0;
   var membercnt = Membercnt;
   var caregivercnt = Caregivercnt;
 
-  var matchingstatics = [{
-    date: "2023/06/21",
-    membname: "홍길동",
-    cgname: "이순신",
-    apprvorrejc: "승인",
-    resn: "-",
-  }
-]
-  var matchcnt = matchingstatics.length;
+  var matchingstatics = Staticlist;
+
   const [modalOpen, setModalOpen] = useState(false);
   const [Id, setId] = useState('');
   const [Selected, setSelected] = useState("2023");
   const [Selected2, setSelected2] = useState("2023");
+  const [Selected3, setSelected3] = useState("2023");
+  
   const handleSelect = (e) => {
     setSelected(e.target.value);
-    console.log(e.target.value);
-
-    if (e.target.value == "2023") {
-      getNewSNbymonth(e.target.value)
-
-    }
-    else if (e.target.value == "2022") {
-      getNewSNbymonth(e.target.value)
-
-    }
-    else if (e.target.value == "2021") {
-      getNewSNbymonth(e.target.value)
-    }
-    else if (e.target.value == "2020") {
-      getNewSNbymonth(e.target.value)
-    }
-    else if (e.target.value == "2019") {
-      getNewSNbymonth(e.target.value)
-    }
+    getNewSNbymonth(e.target.value)
   };
   const handleSelect2 = (e) => {
     setSelected2(e.target.value);
-    if (e.target.value == "2023") {
-      getNewCGbymonth(e.target.value);
-    }
-    else if (e.target.value == "2022") {
-      getNewCGbymonth(e.target.value);
+    getNewCGbymonth(e.target.value);
 
-    }
-    else if (e.target.value == "2021") {
-      getNewCGbymonth(e.target.value);
-    }
-    else if (e.target.value == "2020") {
-      getNewCGbymonth(e.target.value);
-    }
-    else if (e.target.value == "2019") {
-      getNewCGbymonth(e.target.value);
-    }
   };
   //통계 드롭다운임 
   const handleSelect3 = (e) => {
-    setSelected2(e.target.value);
+    setSelected3(e.target.value);
+    getConnectList(e.target.value);
+
  
   };
   //ComplainCaregiverCntDTO
+  const getConnectList = (year) => {
+    axios.get('/api/v1/admin/connect/'+year)
+    .then((response) => {
+      var rstdata = response.data.result;
+      var rstarr = [];
+
+      for (var i=0; i < rstdata.length; i++) {
+
+        rstarr.push(rstdata[i]);
+      }
+
+      setStaticlist(rstarr);
+    }).catch(()=>{
+      alert("로그인에 실패했습니다.")
+  });
+  };
 
   const getcplncareCnt = () => {
     axios.get('/api/v1/admin/work')
     .then((response) => {
 
-      console.log('성공');
-      console.log(JSON.stringify(response.data.result.complainCnt))
-      console.log(JSON.stringify(response.data.result.caregiverCnt))
+
       setNewcargiverpaper(JSON.stringify(response.data.result.caregiverCnt))
       setCstmcomplain(JSON.stringify(response.data.result.complainCnt))
 
@@ -122,12 +114,28 @@ const AdminDashboard = () => {
       alert("로그인에 실패했습니다.")
   });
   };
+
+  const getconnectCnt = async () => {
+    axios.get('/api/v1/admin/connect')
+    .then((response) => {
+
+      console.log('성공');
+      console.log(JSON.stringify(response.data.result));
+      console.log(JSON.stringify(response.data.result.connectAll))
+      setConnectAll(JSON.stringify(response.data.result.connectAll));
+      setConenctSuccess(JSON.stringify(response.data.result.conenctSuccess));
+      setConnectFail(JSON.stringify(response.data.result.connectFail));
+
+
+    }).catch(()=>{
+      alert("로그인에 실패했습니다.")
+  });
+  };
+
   const getNewSNbymonth = (year) => {
     axios.get('/api/v1/admin/seinor/'+year)
     .then((response) => {
-      console.log('성공');
-      console.log(JSON.stringify(response.data.result.thismonth))
-      console.log(JSON.stringify(response.data.result.one))
+
       var one = JSON.stringify(response.data.result.one);
       var two = JSON.stringify(response.data.result.two);
       var three = JSON.stringify(response.data.result.three);
@@ -141,10 +149,11 @@ const AdminDashboard = () => {
       var eleven = JSON.stringify(response.data.result.eleven);
       var twelve = JSON.stringify(response.data.result.twelve);
       var tparr = [one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve];
-      console.log("thisishappy" + tparr);
+
       setMemberdata(tparr);
       setMembercnt(JSON.stringify(response.data.result.seniorAll));
       newmem = response.data.result.thismonth;
+      
     }).catch(()=>{
       alert("로그인에 실패했습니다.")
   });
@@ -153,9 +162,7 @@ const AdminDashboard = () => {
   const getNewCGbymonth = (year) => {
     axios.get('/api/v1/admin/caregiver/'+year)
     .then((response) => {
-      console.log('성공');
-      console.log(JSON.stringify(response.data.result))
-      console.log(JSON.stringify(response.data.result.one))
+
       var one = JSON.stringify(response.data.result.one);
       var two = JSON.stringify(response.data.result.two);
       var three = JSON.stringify(response.data.result.three);
@@ -169,7 +176,7 @@ const AdminDashboard = () => {
       var eleven = JSON.stringify(response.data.result.eleven);
       var twelve = JSON.stringify(response.data.result.twelve);
       var tparr = [one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve];
-      console.log("thisishappy" + tparr);
+
       setCaregiverdata(tparr);
       setCaregivercnt(JSON.stringify(response.data.result.seniorAll));
       newcaremem = response.data.result.thismonth;
@@ -180,62 +187,41 @@ const AdminDashboard = () => {
 
   useEffect(() => {
     getcplncareCnt();
+    getconnectCnt();
     getNewCGbymonth(2023);
     getNewSNbymonth(2023);
-    console.log("useeffect call")
+    getConnectList(2023);
+
     
     //모달쪽인듯
-    // const handleClick = (e) => {
-    //   const clickedElement = e.target;
-    //   const tgclass = clickedElement.className;
+    const handleClick = (e) => {
+      const clickedElement = e.target;
+      const tgclass = clickedElement.className;
       
-    //   if (tgclass.search('css-1nos2st') !== -1) {
-    //     console.log(clickedElement.className);
-    //     const parent = clickedElement.parentNode;
-    //     const grandparent = parent.parentNode;
-    //     const grandparentChildNodes = Array.from(grandparent.childNodes);
-    //     const parentIndex = grandparentChildNodes.indexOf(parent);
-    //     console.log({parentIndex});
-    //     setModalOpen(true);
-    //     setId("test")
-    //   }
-    // };
-    // document.addEventListener('click', handleClick);
-    // return () => {
-    //   document.removeEventListener('click', handleClick);
-    // };
+      if (tgclass.search('css-1nos2st') !== -1) {
+        console.log(clickedElement.className);
+        const parent = clickedElement.parentNode;
+        const grandparent = parent.parentNode;
+        const grandparentChildNodes = Array.from(grandparent.childNodes);
+        const parentIndex = grandparentChildNodes.indexOf(parent);
+        console.log({parentIndex});
+        setModalOpen(true);
+        setId("test")
+      }
+    };
+    document.addEventListener('click', handleClick);
+    return () => {
+      document.removeEventListener('click', handleClick);
+    };
   }, []);
   
   useEffect(() => {
 
-    console.log("call")
     
   }, [Caregiverdata]);
-
-  
-
-
-  
-  function appdecmaker(arr) {
-    var temparr = arr;
-    for (let i = 0; i < temparr.length; i++) {
-      var tg = arr[i].apprvorrejc;
-      if (tg == "승인") {
-
-        matchappr += 1;
-      }
-      else if (tg == "거절") {
-  
-        matchrejc += 1
-      }
-    }
-  }
-
-  appdecmaker(matchingstatics);
-    
   return (
     <ThemeProvider theme={theme}>
-      {modalOpen && Id && <AdminModal setModalOpen={setModalOpen} />}
+      {modalOpen && Id && <AdminModal setModalOpen={setModalOpen}  />}
       <CssBaseline />
       <div className='adwrap'>
           <Adheader />
@@ -243,7 +229,7 @@ const AdminDashboard = () => {
               <div className='adctfourdiv'>
                   <div className='adcdiv0 adcchild'>
                     <p><span>현재 고객 : {membercnt}명 </span><span>요양사 회원 :{caregivercnt}명 </span><span className='admingolist' onClick={() => navigate('/admin/list')}> 요양사 승인대기 : <span className='adminred'>{newcargiverpaper}</span></span><span className='admincomplain' onClick={() => navigate('/admin/complain')}>불만사항 대기 : <span className='adminred' >{cstmcomplain}</span></span></p>
-                    <p><span>신규가입 고객 : {newmem}명 </span><span>신규가입 요양사 :{newcaremem}명 </span><span>금월 매칭수 : {matchcnt} </span><span>금월 매칭 성공 : {matchappr}</span><span>금월 매칭 실패 : {matchrejc}</span></p>
+                    <p><span>신규가입 고객 : {newmem}명 </span><span>신규가입 요양사 :{newcaremem}명 </span><span>금월 매칭수 : {connectAll}</span><span>금월 매칭 성공 : {conenctSuccess}</span><span>금월 매칭 실패 : {connectFail}</span></p>
                   </div>
                   <div className='adcdiv1 adcchild'>
                   <DefaultLineChart
@@ -310,28 +296,29 @@ const AdminDashboard = () => {
                       </div>
                     </div>
                   </div>
-                  <select className='admindropdownleft' onChange={handleSelect3} value={Selected}>
+                  <select className='admindropdownleft' onChange={handleSelect3} value={Selected3}>
                       <option value={"2019"}>2019년</option>
                       <option value={"2020"}>2020년</option>
                       <option value={"2021"}>2021년</option>
                       <option value={"2022"}>2022년</option>
                       <option value={"2023"}>2023년</option>
                     </select>
-                  <DataTable
+                    <DataTable
                     icon={{ color: "info", component: "leaderboard" }}
                     title="신규 회원수 동향"
                     description="월별(명)"
                     table={{
                       columns: [
                         { Header: "일자", accessor: "date", width: "15%" },
-                        { Header: "회원명", accessor: "membname", width: "15%" },
-                        { Header: "요양사", accessor: "cgname", width: "15%" },
-                        { Header: "승인/거절", accessor: "apprvorrejc", width: "12%" },
-                        { Header: "사유", accessor: "resn", width: "12%" },
+                        { Header: "회원명", accessor: "caregiverId", width: "15%" },
+                        { Header: "요양사", accessor: "seniorId", width: "15%" },
+                        { Header: "승인/거절", accessor: "choose", width: "12%" },
+                        { Header: "사유", accessor: "reason", width: "12%" },
                       ],
                       rows: matchingstatics
                     }}
                   />
+         
                   </div>
               </div>
           </div>
