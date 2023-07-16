9c1cad45753576ddf093db02e881d00b3e238f5a
KimHyeonJin <hyeonjin9819@gmail.com>
2023-07-16 20:27:30 +0900

    fix: App.js 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ import FindHelperPage from './pages/FindHelper/FindHelperPage';
 import HelperProfilePage from './pages/HelperProfile/HelperProfilePage';
 import HelperDetailProfilePage from './pages/HelperDetailProfile/HelperDetailProfilePage';
 import CaregiverMyPage from './pages/CaregiverMyPage';
+import AdminDashboard from 'pages/AdminDashboard';
+import ComplainPage from 'pages/Complain/ComplainPage';
+import Jmpagination from 'pages/AdminDetail/Jmpagination';
+import Kjmadmindetail from 'pages/AdminDetail/AdminDetail';
 
 const App = () => {
   return (
@@ -32,6 +36,12 @@ const App = () => {
       <Route path="/caregivermain/caregivermypage" element={<CaregiverMyPage />} />
       <Route path="/caregivermain/view" element={<View />} />
       <Route path="/caregivermain/cal" element={<Cal />} />
+
+      <Route path="/admin/home" element={<AdminDashboard/>}/>
+      <Route path="/admin/complain" element={<ComplainPage/>}/>
+      <Route path="/admin/list" element={<Kjmadmindetail/>}/>
+
+
     </Routes>
   );
 };
